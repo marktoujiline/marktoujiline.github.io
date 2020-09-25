198f71b5f3816f6c80d34f1936e03ede131dd2b1
Mark Toujiline <mark.toujiline@gmail.com>
2020-09-24 19:23:11 -0700

    Updates

---
+++ precache-manifest.a6622489c46eddcad5307cc70584cae4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9952f28e965d4fa934f03e6d844d141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc630823548dcfc594a1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "93d235137e320af72771",
+    "url": "/static/js/2.23f836b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.23f836b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc630823548dcfc594a1",
+    "url": "/static/js/main.15f47c70.chunk.js"
+  },
+  {
+    "revision": "40ecd06f33f96d11bb2b",
+    "url": "/static/js/runtime-main.9c2ed971.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
